38fc97f422b61782774e5e29c4e300230e1171da
tommycacatian <tommcacatian2>
2016-02-16 21:45:00 +0800

    update alt home image

---
+++ tommy/js/mainbg.js
@@ -0,0 +1,10 @@
+function mainAlt() {
+   var w = window.innerWidth
+			|| document.documentElement.clientWidth
+			|| document.body.clientWidth;
+			var w1=0;
+			if(w<760){
+				document.getElementById("homediv").style.background = "black url('tommy/acimg/achomealt.png') no-repeat right top";	
+			}
+			
+}
